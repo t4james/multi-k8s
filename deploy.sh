11280a0ccf59077387e06bf47317e4ec56eb936f
t4james <t4james@LTS18.04.1-Base>
2019-05-22 15:40:06 +1200

    modify deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t t4james/multi-client:latest -t t4james/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t t4james/multi-server:latest -t t4james/multi-server:$SHA -f ./client/Dockerfile ./server
-docker build -t t4james/multi-worker:latest -t t4james/multi-worker:$SHA -f ./client/Dockerfile ./worker
+docker build -t t4james/multi-server:latest -t t4james/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t t4james/multi-worker:latest -t t4james/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push t4james/multi-client:latest
 docker push t4james/multi-server:latest

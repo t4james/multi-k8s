4993d5476aa39a27adb6eeb6f46183cbdde2e233
t4james <t4james@LTS18.04.1-Base>
2019-05-22 15:28:07 +1200

    added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t t4james/multi-client:latest -t t4james/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t t4james/multi-server:latest -t t4james/multi-server:$SHA -f ./client/Dockerfile ./server
+docker build -t t4james/multi-worker:latest -t t4james/multi-worker:$SHA -f ./client/Dockerfile ./worker
+
+docker push t4james/multi-client:latest
+docker push t4james/multi-server:latest
+docker push t4james/multi-worker:latest
+
+docker push t4james/multi-client:$SHA
+docker push t4james/multi-server:$SHA
+docker push t4james/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=t4james/multi-server:$SHA
+kubectl set image deployments/client-deployment client=t4james/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=t4james/multi-worker:$SHA
